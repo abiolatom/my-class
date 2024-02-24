3fc2296d162488575a44682eb7b5c27069490df7
abiolatom <107856136+abiolatom@users.noreply.github.com>
2024-02-24 16:41:37 +0200

    on it

---
+++ src/app/ui/users/new-user.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+
+export default function form() {
+    <form>
+        <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+            Choose customer
+          </label>
+    </form>
+}
