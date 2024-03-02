254d55ea7583363c3995d88310a53aadd36fe9c7
abiolatom <107856136+abiolatom@users.noreply.github.com>
2024-03-02 14:03:20 +0200

    moving to aesthethic

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -10,7 +10,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ onSubmit }) => {
     email: "",
     phoneNumber: "",
     country: "",
-      city: "",
+    city: "",
     skillLevel: "",
     additionalInfo: "",
   });
@@ -24,113 +24,131 @@ const ContactForm: React.FC<ContactFormProps> = ({ onSubmit }) => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-   onSubmit(formData);
+    onSubmit(formData);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <section>
-        <div>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Enter your name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email address"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="tel"
-            id="phoneNumber"
-            name="phoneNumber"
-            placeholder="Phone Number: Intl format"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="country"
-            name="country"
-            placeholder="Enter current country of residence"
-            value={formData.country}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="Enter city"
-            value={formData.city}
-            onChange={handleChange}
-            required
-          />
-        </div>
-      </section>
-      <section>
-        {" "}
-        <div>
-          <h1> Finnish Skills</h1>
-              </div>
-              
-              <div>
-              <label htmlFor="skillLevel">Additional Information:</label>
-              <select
-              id="customer"
-              name="customerId"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+    <div min-h-screen items-center justify-center>
+      <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+        <section className="flex flex-col space-y-2">
+          <div className="flex flex-col">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="rounded-md border border-gray-300 
+            px-3 py-2 focus:outline-none focus:border-sky-500"
+              placeholder="Enter your name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-sm font-medium">
+              Enter your email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="your.email@example.com"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:border-sky-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="phoneNumber" className="text-sm font-medium">
+              Phone Number: Intl format
+            </label>
+            <input
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              placeholder="Phone Number"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+              className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:border-sky-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="country" className="text-sm font-medium">
+              Enter current country of residence
+            </label>
+            <input
+              type="text"
+              id="country"
+              name="country"
+              placeholder="Country"
+              value={formData.country}
+              onChange={handleChange}
+              required
+              className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:border-sky-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="city" className="text-sm font-medium">
+              Enter city
+            </label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              placeholder="City"
+              value={formData.city}
+              onChange={handleChange}
+              required
+              className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:border-sky-500"
+            />
+          </div>
+        </section>
+        <section className="flex flex-col space-y-2">
+          <div className="flex flex-col">
+            <h1 className="text-xl font-bold">Finnish Skills</h1>
+            <label htmlFor="skillLevel" className="text-sm font-medium mt-2">
+              Define your Finnish Skill Level
+            </label>
+            <select
+              id="skillLevel"
+              name="skillLevel"
+              className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:border-sky-500 appearance-none"
               defaultValue=""
             >
               <option value="" disabled>
                 Define your Finnish Skill Level
               </option>
               {skillLevels.map((level) => (
-                <option key={level} value={formData.skillLevel}>
+                <option key={level} value={level}>
                   {level}
                 </option>
               ))}
-            </select> <input
-            type="text"
-            id="country"
-            name="country"
-            placeholder="Enter current country of residence"
-            value={formData.country}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="additionalInfo">Additional Information:</label>
-          <textarea
-            id="additionalInfo"
-            name="additionalInfo"
-            value={formData.additionalInfo}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-      </section>
-      <button type="submit">Submit</button>
-    </form>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="additionalInfo" className="text-sm font-medium">
+              Additional Information:
+            </label>
+            <textarea
+              id="additionalInfo"
+              name="additionalInfo"
+              value={formData.additionalInfo}
+              onChange={handleChange}
+              required
+              className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:border-sky-500 resize-none"
+            ></textarea>
+          </div>
+        </section>
+        <button
+          type="submit"
+          className="px-4 py-2 rounded-md bg-sky-500 text-white font-medium hover:bg-sky-700"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
